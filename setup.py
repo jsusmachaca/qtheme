c271aaf962e2f7bcaa581122fac8b37558561fc4
jsusmachaca <falcorgd@gmail.com>
2024-10-06 23:20:11 -0500

    change version on setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="qtheme",
-    version="1.4",
+    version="1.5",
     description="Tools for management qtile environment",
     long_description=README,
     long_description_content_type="text/markdown",
